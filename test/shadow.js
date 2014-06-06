f1f3f907ddbfd94ff32583639b0671b4edcc192a
Jan Marsch <jama@keks.com>
2014-06-06 15:30:25 +0200

    calc fix

--- test/shadow.js
+++ test/shadow.js
@@ -1,9 +1,11 @@
 function shadow(date, center, polygon, height) {
   var sunpos = suncalc(date, center.y, center.x);
 
+  var gm = geometers(center.y);
+
   var dir = {
-    x: Math.cos(sunpos.azimuth) / Math.tan(sunpos.altitude),
-    y: Math.sin(sunpos.azimuth) / Math.tan(sunpos.altitude)
+    x: Math.cos(sunpos.azimuth) / Math.tan(sunpos.altitude) / gm.x,
+    y: Math.sin(sunpos.azimuth) / Math.tan(sunpos.altitude) / gm.y
   };
 
   var
@@ -12,8 +14,6 @@ function shadow(date, center, polygon, height) {
     _x1, _y1, _x2, _y2,
     res = [];
 
-  var gm = geometers(center.y);
-
   mode = null;
   for (var i = 0, il = polygon.length-1; i < il; i++) {
     x1 = polygon[i].x;
@@ -21,11 +21,11 @@ function shadow(date, center, polygon, height) {
     x2 = polygon[i+1].x;
     y2 = polygon[i+1].y;
 
-    _x1 = x1 + dir.x*height / gm.y;
-    _y1 = y1 + dir.y*height / gm.x;
+    _x1 = x1 + dir.x*height;
+    _y1 = y1 - dir.y*height;
 
-    _x2 = x2 + dir.x*height / gm.y;
-    _y2 = y2 + dir.y*height / gm.x;
+    _x2 = x2 + dir.x*height;
+    _y2 = y2 - dir.y*height;
 
     // mode 0: floor edges, mode 1: roof edges
     if ((x2-x1) * (_y1-y1) > (_x1-x1) * (y2-y1)) {
@@ -55,5 +55,4 @@ function shadow(date, center, polygon, height) {
   }
 
   return { type:'LineString', coordinates:coordinates };
-  //return res;
 }
